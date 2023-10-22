572a6beeff9677aead44b6db08ae515da57b434f
karnati007 <karng01@pfw.edu>
2023-10-22 14:25:43 -0400

    HomePage Footer Updated

--- rail_car_care/src/components/NavigationBar.jsx
+++ rail_car_care/src/components/NavigationBar.jsx
@@ -6,11 +6,11 @@ import Button from 'react-bootstrap/Button';
 import Logo from "../images/Logo.PNG";
 import './NavigationBar.css';
 
-function NavBar({ onLogout }) {
+function NavigationBar({ onLogout }) {
 
     return (
-        <Navbar className="navigation" bg="dark" variant="dark" expand="lg" fixed="top">
-            <Navbar.Brand className="brand"><img className="image-logo" src={Logo} alt='RailCarCareLogo'/>RailCarCare</Navbar.Brand>
+        <Navbar className="navigation" bg="dark"  expand="lg" fixed="top">
+            <Navbar.Brand className="brand"><img className="image-logo" src={Logo} />RailCarCare</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="custom-nav"> {/* Use ml-auto to float navigation items right */}
@@ -20,9 +20,10 @@ function NavBar({ onLogout }) {
                     <Link to="/profile" className="nav-link">Profile</Link>
                     <Button variant="danger" onClick={onLogout}>Logout</Button>
                 </Nav>
+               
             </Navbar.Collapse>
         </Navbar>
     );
 }
 
-export default NavBar;
+export default NavigationBar;
